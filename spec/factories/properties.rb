f1fcb1becc1d5a827616e1c280c304849cecfd32
memoxmrdl <jmemox@gmail.com>
2017-02-16 21:05:17 -0600

    Apply validations required to Property tests

--- spec/factories/properties.rb
+++ spec/factories/properties.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :property do
-    
+    type 'house'
+    title { Faker::Lorem.paragraph.first(15) }
+    address { Faker::Lorem.paragraph.first(30) }
+    zipcode { Faker::Address.zip_code.first(5) }
+    country 'mx'
+    notes { Faker::Lorem.paragraph }
   end
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Property, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_db_column(:id).of_type(:integer) }
+  it { is_expected.to have_db_column(:type).of_type(:integer) }
+  it { is_expected.to have_db_column(:address).of_type(:string) }
+  it { is_expected.to have_db_column(:zipcode).of_type(:string) }
+  it { is_expected.to have_db_column(:country).of_type(:string) }
+  it { is_expected.to have_db_column(:notes).of_type(:text) }
+  it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+  it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+
+  it { is_expected.to validate_presence_of(:type) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:address) }
+  it { is_expected.to validate_presence_of(:zipcode) }
+  it { is_expected.to validate_presence_of(:country) }
+
+  it { is_expected.to validate_length_of(:title).is_at_least(15).is_at_most(45) }
+  it { is_expected.to validate_length_of(:address).is_at_least(30).is_at_most(140) }
+  it { is_expected.to validate_length_of(:notes).is_at_most(300) }
+  it { is_expected.to validate_length_of(:country).is_equal_to(2) }
+  it { is_expected.to define_enum_for(:type) }
+  it { is_expected.to allow_value('99999', '11111').for(:zipcode) }
 end
