64c58872f9c3263050831304d1bf9ff1d12248d7
memoxmrdl <jmemox@gmail.com>
2017-02-17 00:16:31 -0600

    Create PropertiesController#create & rspec tests

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -11,9 +11,23 @@ class PropertiesController < ApplicationController
     render json: @property
   end
 
+  def create
+    @property = Property.new(property_params)
+
+    if @property.save
+      render json: @property, status: :created, location: @property
+    else
+      render json: { response: false, message: @property.errors.full_messages.join(', ') }, status: :bad_request
+    end
+  end
+
   private
 
   def find_property
     @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:title, :address, :zipcode, :country, :notes, :house_type)
   end
 end
