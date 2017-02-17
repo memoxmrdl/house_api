6b9b4141bed14a2c306cce1e8ec0412c15dfb303
memoxmrdl <jmemox@gmail.com>
2017-02-16 21:05:08 -0600

    Apply validations required to Property

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,15 @@
 class Property < ApplicationRecord
+  validates :type,
+            :title,
+            :address,
+            :zipcode,
+            :country, presence: true
+
+  validates :country, length: { is: 2 }
+  validates :notes, length: { maximum: 300 }
+  validates :title, length: { minimum: 15, maximum: 45 }
+  validates :address, length: { minimum: 30, maximum: 140 }
+  validates :zipcode, numericality: true, format: { with: /\A\d{5}-\d{4}|\A\d{5}\z/ }
+
+  enum type: [:house, :departament]
 end
