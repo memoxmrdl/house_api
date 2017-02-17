8de06e8a9bddb71697b88d77c5144c1de0bc8df1
memoxmrdl <jmemox@gmail.com>
2017-02-16 23:57:19 -0600

    Create PropertiesController#show & rspec tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found
+    render json: {}, status: :not_found
+  end
+
+  def bad_request
+    render json: {}, status: :bad_request
+  end
 end
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,7 +1,19 @@
 class PropertiesController < ApplicationController
+  before_action :find_property, except: [:index, :create]
+
   def index
     @properties = Property.all
 
     render json: @properties
+  end
+
+  def show
+    render json: @property
+  end
+
+  private
+
+  def find_property
+    @property = Property.find(params[:id])
   end
 end
